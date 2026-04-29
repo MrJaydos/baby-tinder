5c8cdce765fcfe62ef4a9f72c4b7f88d26305f39
Jayden Dickinson <jaydend93@gmail.com>
2026-04-29 20:07:05 +1000

    More comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import the create app function from the website package defined by the folder.
 from website import create_app
 
+# Stores the flask app instance from the factory function so it can be referenced later.
 app = create_app()
 
 # Only runs when this file is executed direct, not when imported as a module.
