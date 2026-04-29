279ac36249927aee8934099c6b7744152632d488
Jayden Dickinson <jaydend93@gmail.com>
2026-04-29 20:05:01 +1000

    Added comments explaining things

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# Import the create app function from the website package defined by the folder.
 from website import create_app
 
 app = create_app()
 
+# Only runs when this file is executed direct, not when imported as a module.
 if __name__ == '__main__':
     app.run(debug=True)
